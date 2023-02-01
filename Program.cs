0bf128f015d95471fb811002f409db783893bd47
Grigorev.Igor <vasp5ky5@yandex.ru>
2023-02-01 10:59:53 +0300

    переделано решение задачи

--- Program.cs
+++ Program.cs
@@ -1,22 +1,24 @@
-﻿string[] array =
+﻿string[] array1 = new string[5] {"123", "23", "hello", "world", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
 {
-    "a",
-    "bb",
-    "ccc",
-    "dddd",
-    "eeeee",
-    "z"
-};
- 
-var result = new string[array.Length];
-var realSize = 0;
-foreach (var value in array)
-{
-    if (value.Length >= 3)
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
     {
-        result[realSize] = value;
-        realSize++;
+    if(array1[i].Length >= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
     }
 }
- 
-Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
